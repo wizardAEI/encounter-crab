adeac73375924fa59fb04b914cfd05256431fb66
wangdejiang <wangdejiang@baidu.com>
2024-12-10 11:26:57 +0800

    onwership

---
+++ src/4. Understanding Ownership/4.1. What is Ownership.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // ownership
+    let s1 = String::from("hello");
+    let s2 = s1; // s1's value move
+    println!("s2: {s2}");
+    let s3 = take_ownership(s2); // s2's value move into the function and so is no longer valid here
+    // println!("s1: {s1}, s2: {s2}"); // borrow of moved value: `s1`
+    println!("s3: {s3}")
+}
+
+fn take_ownership(s: String) -> String {
+    println!("s: {s}");
+    s
+}
